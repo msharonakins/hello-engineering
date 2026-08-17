35ce973897a0b765a36dc3366b3ac1576a0e1c31
Sharon Akinlaja <msharonakinlaja@gmail.com>
2026-08-17 20:48:52 +0200

    Add Git workflow practice message

--- hello.py
+++ hello.py
@@ -2,4 +2,5 @@ print("Hello, Engineering!")
 print("Currently learning software engineering, i'm having fun!")
 print("This change was made on the branch")
 print("Introduction phase 0 is now complete!")
+print("Learning professional Git workflow")
 
