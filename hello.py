a234af91aabddcc018d5dafa19788a34f309a62f
Sharon Akinlaja <msharonakinlaja@gmail.com>
2026-08-17 15:37:28 +0200

    Add phase 0 branch message

--- hello.py
+++ hello.py
@@ -1,4 +1,5 @@
 print("Hello, Engineering!")
 print("Currently learning software engineering, i'm having fun!")
 print("This change was made on the branch")
+print("Introduction phase 0 is now complete!")
 
